34417e8d91d115c08dde5a15d35d5ff0e872b37b
teyim <teyimasobo@gmail.com>
2022-03-25 12:18:28 +0200

    Modify next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['cdn.shopify.com', 's3.amazonaws.com', 'i2.wp.com', 'www.creativeboom.com', 'cdn11.bigcommerce.com', 'images.unsplash.com', 'images.ctfassets.net', 'wac-cdn.atlassian.com', 'www.riseart.com', 'i.pinimg.com', 'media.istockphoto.com', 'mymodernmet.com'],
+  },
 }
 
 module.exports = nextConfig
