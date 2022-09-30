775a1cc09e2a21258a29ec2b8f45394fc560b28c
teyim <teyimasobo@gmail.com>
2022-09-30 20:35:34 +0100

    perf:add client side arts fetching

--- pages/search/[param].js
+++ pages/search/[param].js
@@ -1,85 +1,46 @@
-import React from 'react'
+import { useEffect } from 'react'
 import { AiOutlineClose } from 'react-icons/ai'
 import FilterTab from 'components/ui/filtertab'
 import Select from 'react-select'
 import { useRouter } from 'next/router'
 import ArtsList from 'components/ui/artslist'
-import { useCart } from 'helpers/store'
+import { useArtsStore, useCart } from 'helpers/store'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from 'helpers/firebase/clientApp'
+import shallow from 'zustand/shallow'
+import LoadingSpinner from 'HOC/loadingSpinner'
 
 function ArtSearch() {
 	const cartItems = useCart((state) => state.cartItems)
+	const { arts, setArts, isLoading, setIsLoading } = useArtsStore(
+		(state) => ({
+			arts: state.arts,
+			setArts: state.setArts,
+			isLoading: state.isLoading,
+			setIsLoading: state.setIsLoading,
+		}),
+		shallow
+	)
 	const addCartItem = useCart((state) => state.addCartItem)
 
 	const router = useRouter()
 	const { query } = router.query
 	console.log(query)
 
-	const paintings = [
-		{
-			imageUrl:
-				'http://i2.wp.com/zet.gallery/blog/wp-content/uploads/2016/02/Vicent-Van-Gogh-Starry-Night-Famous-Oil-Paintings-www.shairart.com_.jpg?fit=1280%2C1014',
-			name: 'Wallowing breeze',
-			artist: 'Hettie Richardsakjkjakd',
-			type: 'oil in canvas',
-			dimensions: '26in x 36in',
-			year: '2008',
-			price: 620,
-			id: 1,
-		},
-		{
-			imageUrl:
-				'https://cdn11.bigcommerce.com/s-x49po/products/5684/images/14277/52Figure40__50289.1506574078.500.659.JPG?c=2',
-			name: 'Wallowing bkakdjalkd',
-			artist: 'Hettie Richards',
-			type: 'oil in canvas',
-			dimensions: '26in x 36in',
-			year: '2008',
-			price: 620,
-			id: 2,
-		},
-		{
-			imageUrl:
-				'https://www.creativeboom.com/uploads/articles/a4/a41200a5667f1051d5d8f508d4b15ae4393b7f7d_810.jpg',
-			name: 'Wallowing breezeakjkald',
-			artist: 'Hettie Richards',
-			type: 'oil in canvas',
-			dimensions: '26in x 36in',
-			year: '2008',
-			price: 620,
-			id: 3,
-		},
-		{
-			imageUrl:
-				'http://cdn.shopify.com/s/files/1/0017/9657/1249/products/Landscape-painting-of-tropical-waterfall-painting-on-canvas-by-Karen-Whitworth-Healing-Retreat.jpg?v=1612313935',
-			name: 'Wallowing breezeakjjda',
-			artist: 'Hettie Richards',
-			type: 'oil in canvas',
-			dimensions: '26in x 36in',
-			year: '2008',
-			price: 620,
-			id: 4,
-		},
-		{
-			imageUrl:
-				'http://i2.wp.com/zet.gallery/blog/wp-content/uploads/2016/02/Vicent-Van-Gogh-Starry-Night-Famous-Oil-Paintings-www.shairart.com_.jpg?fit=1280%2C1014',
-			name: 'Wallowing breezehjahd',
-			artist: 'Hettie Richards',
-			type: 'oil in canvas',
-			dimensions: '26in x 36in',
-			year: '2008',
-			price: 620,
-			id: 5,
-		},
-	]
-
-	const categoryOptions = [
-		{ value: 'all', label: 'All' },
-		{ value: 'painting', label: 'Painting' },
-		{ value: 'sculpting', label: 'Sculpting' },
-		{ value: 'drawing', label: 'Drawing' },
-		{ value: 'digitalArt', label: 'Digital Arts' },
-		{ value: 'photography', label: 'Photography' },
-	]
+	useEffect(() => {
+		const fetchArts = async () => {
+			setIsLoading(true)
+			const artsCollection = collection(db, 'arts')
+			const artsSnapShot = await getDocs(artsCollection)
+			let arts = []
+			artsSnapShot.docs.forEach((doc) => {
+				arts.push({ ...doc.data(), id: doc.id })
+			})
+			setArts(arts)
+			setIsLoading(false)
+		}
+		fetchArts()
+	}, [])
 
 	const addToCartHandler = (art) => {
 		const itemExist = cartItems.some((item) => item.id === art.id)
@@ -130,7 +91,7 @@ function ArtSearch() {
 				</div>
 				<div className='md:w-[70%] py-6 px-10'>
 					<div className='md:w-1/5 '>
-						<Select options={categoryOptions} placeholder='Sort by' />
+						<Select options={[]} placeholder='Sort by' />
 					</div>
 				</div>
 			</div>
@@ -142,12 +103,16 @@ function ArtSearch() {
 					<FilterTab name='Year' />
 				</div>
 				<div className='md:w-[70%] px-6 py-6 overflow-y-scroll md:flex md:flex-wrap md:-mx-4 '>
-					<ArtsList
-						paintings={paintings}
-						addToCartHandler={addToCartHandler}
-						imageSize='w-[200px] h-[200px]'
-						customStyle='md:py-4 md:px-4 md:w-1/3'
-					/>
+					{isLoading ? (
+						<LoadingSpinner size='text-4xl' />
+					) : (
+						<ArtsList
+							arts={arts}
+							addToCartHandler={addToCartHandler}
+							imageSize='w-[200px] h-[200px]'
+							customStyle='md:py-4 md:px-4 md:w-1/3'
+						/>
+					)}
 				</div>
 			</div>
 		</div>
