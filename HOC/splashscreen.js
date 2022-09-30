690b4d61bae3505934cd5cf0a5297701980ecf48
teyim <teyimasobo@gmail.com>
2022-09-30 20:31:08 +0100

    style:modify splashscreen component

--- HOC/splashscreen.js
+++ HOC/splashscreen.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import LoadingSpinner from './loadingSpinner'
 
 function SplashScreen() {
 	return (
 		<div className='w-screen h-screen flex justify-center font-Vollkorn'>
 			<div className='my-auto text-4xl md:text-5xl font-thin animate-fadein'>
+				<LoadingSpinner size='text-4xl' />
 				Cultured kid.
 			</div>
 		</div>
